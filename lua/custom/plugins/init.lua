466310e519cb13da2434d34f69472dadb4e7ae07
Arshia Eskandari <arshia.eskandari.3000@gmail.com>
2025-11-18 09:36:39 -0500

    Add ny nvim config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^6',
+    ft = 'rust',
+    lazy = false,
+  },
+}
